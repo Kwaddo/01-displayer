0416c9250c28f768a5dc28837d6d17f4b7eb9a30
ak1 <ali842977@gmail.com>
2025-01-16 18:52:57 +0300

    Finally fixed my issues!

--- utils/db.ts
+++ utils/db.ts
@@ -7,9 +7,16 @@ export async function getData() {
     return data;
 }
 
-export async function getUserByToken(token: string) {
-    const result = await sql`
-      SELECT * FROM users WHERE token = ${token}
-    `;
-    return result[0];
+export async function getUserByUserID(userID: string) {
+    try {
+        const result = await sql`
+        SELECT * FROM users WHERE user_id = ${userID}
+      `;
+
+        // Check if a user was found, otherwise return null
+        return result.length > 0 ? result[0] : null;  // Return the first user or null if not found
+    } catch (error) {
+        console.error('Error fetching user by user_id:', error);
+        throw new Error('Error querying the database');
+    }
 }
